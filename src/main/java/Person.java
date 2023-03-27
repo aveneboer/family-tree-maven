36b33ead36a2c26b13f149374e056592a3619c0c
aveneboer <anoukveneboer@hotmail.com>
2023-03-27 16:49:18 +0200

    alles

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,2 +1,146 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Person {
+
+    private String name;
+    private String middleName;
+    private String lastName;
+    private String sex;
+    int age;
+    private Person mother;
+    private Person father;
+    private ArrayList<Person> siblings;
+    private ArrayList<Person> children;
+    private ArrayList<Pet> pets;
+
+    public Person(String name, String lastName, int age, String sex) {
+        this.name = name;
+        this.lastName = lastName;
+        this.sex = sex;
+        this.age = age;
+        this.children = new ArrayList<>();
+        this.siblings = new ArrayList<>();
+        this.pets = new ArrayList<>();
+    }
+
+    public Person(String name, String middleName, String lastName, int age, String sex) {
+        this(name, lastName, age, sex);
+        this.middleName = middleName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person getMother() {
+        return mother;
+    }
+
+    public void setMother(Person mother) {
+        this.mother = mother;
+    }
+
+    public Person getFather() {
+        return father;
+    }
+
+    public void setFather(Person father) {
+        this.father = father;
+    }
+
+    public ArrayList<Person> getSiblings() {
+        return siblings;
+    }
+
+    public void setSiblings(ArrayList<Person> siblings) {
+        this.siblings = siblings;
+    }
+
+    public ArrayList<Person> getChildren() {
+        return children;
+    }
+
+    public void setChildren(ArrayList<Person> children) {
+        this.children = children;
+    }
+
+    public ArrayList<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(ArrayList<Pet> pets) {
+        this.pets = pets;
+    }
+
+
+    public void addParents(Person mother, Person father) {
+        this.mother = mother;
+        this.father = father;
+        mother.getChildren().add(this);
+        father.getChildren().add(this);
+    }
+
+    public void addChild(Person child) {
+        children.add(child);
+        child.setMother(this);
+        child.setFather(this);
+    }
+
+    public void addPet(Pet pet) {
+        pets.add(pet);
+        pet.setOwner(this);
+    }
+
+    //addSiblings
+    public void addSibling(Person sibling) {
+        siblings.add(sibling);
+        sibling.getSiblings().add(this);
+    }
+
+    // Get grandchildren
+    public ArrayList<Person> getGrandChildren() {
+        ArrayList<Person> grandchildren = new ArrayList<>();
+        for (Person child : children) {
+            grandchildren.addAll(child.getChildren());
+        }
+        return grandchildren;
+    }
+
 }
--- src/test/java/PetTest.java
+++ src/test/java/PetTest.java
@@ -1,4 +1,36 @@
 import static org.junit.jupiter.api.Assertions.*;
-class PetTest {
-  
+
+
+import org.junit.Test;
+
+public class PetTest {
+
+    @Test
+    public void testConstructor() {
+        Pet pet = new Pet("Fluffy", 5, "Dog");
+        assertEquals("Fluffy", pet.getName());
+        assertEquals(5, pet.getAge());
+        assertEquals("Dog", pet.getSpecies());
+    }
+
+    @Test
+    public void testSetters() {
+        Pet pet = new Pet("Fluffy", 5, "Dog");
+        pet.setName("Max");
+        pet.setAge(7);
+        pet.setSpecies("Cat");
+        assertEquals("Max", pet.getName());
+        assertEquals(7, pet.getAge());
+        assertEquals("Cat", pet.getSpecies());
+    }
+
+    @Test
+    public void testOwner() {
+        Pet pet = new Pet("Fluffy", 5, "Dog");
+        Person owner = new Person("John", "Doe", 40, "male");
+        pet.setOwner(owner);
+        assertEquals(owner, pet.getOwner());
+    }
+
 }
+
